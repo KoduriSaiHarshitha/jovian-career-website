9aeae71421d55127fb7691509206f3e9170e6c11
KoduriSaiHarshitha <141612920+KoduriSaiHarshitha@users.noreply.github.com>
2023-08-08 19:57:04 +0000

    added explore other jobs button

--- app.py
+++ app.py
@@ -23,25 +23,26 @@ def show_job(id):
     return "Not Found", 404
 
   return render_template("jobpage.html", job=job)
-  
+
+
 @app.route("/api/job/<id>")
 def show_job_json(id):
   job = load_job_from_db(id)
   return jsonify(job)
 
+
 @app.route("/job/<id>/apply", methods=['POST'])
 def apply_to_job(id):
-    data = request.form
-    job = load_job_from_db(id)
-    if job is None:
-        return "Not Found", 404
+  data = request.form
+  job = load_job_from_db(id)
+  if job is None:
+    return "Not Found", 404
 
-    add_application_to_db(id, data)
+  add_application_to_db(id, data)
 
-    return render_template("application_submitted.html",
-                           application=data,
-                           job=job)
-
+  return render_template("application_submitted.html",
+                         application=data,
+                         job=job)
 
 
 if __name__ == "__main__":
